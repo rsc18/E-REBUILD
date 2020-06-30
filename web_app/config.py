e5c22e3d94c98efd598ed2f4fcf41e6ce5783e72
rsc18 <43114948+rsc18@users.noreply.github.com>
2020-06-29 20:12:08 -0400

    Update config.py

--- web_app/config.py
+++ web_app/config.py
@@ -6,6 +6,9 @@ SECRET_KEY = secrets.token_urlsafe(16)   # '23422.14432017405'
 # Define the database - we are working with
 # SQLite for this example
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///erebuild.sqlite'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/erebuild_dup'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/erebuild_dup'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test:test@localhost/erebuild'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
+
+
